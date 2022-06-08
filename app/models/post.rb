32311d00f2d65f6d013ede5b4e678f140e66edad
JosephShin <joespam9999@gmail.com>
2022-06-08 11:14:13 -0400

    commit tree 9371658da96e071af6ba667d6ee2d4c7880a0f55

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,25 @@
 class Post < ApplicationRecord
+    
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :clickbait?
+
+    CLICKBAIT_PATTERNS = [
+      /Won't Believe/i,
+      /Secret/i,
+      /Top \d/i,
+      /Guess/i
+    ]
+  
+    def clickbait?
+      if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+        errors.add(:title, "must be clickbait")
+      end
+    # if !title.include?("Won't Believe") || !title.include?("Secret") || !title.include?("Top") || !title.include?("Top") || !title.include?("Guess")
+    #     errors.add(:title, "must be clickbait")
+    # end
+
+    end
 end
